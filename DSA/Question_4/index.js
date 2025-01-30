80b051d6fc9042711ef020e3b9487edbce11fb51
Prakhar Pandey <148262960+Prakhar210804@users.noreply.github.com>
2025-01-30 22:07:00 +0530

    Add files via upload

---
+++ DSA/Question_4/index.js
@@ -0,0 +1,11 @@
+// <!-- 4. Reverse a String -->
+function reverseString(str) {
+    
+  let r = "";
+  for(let i =str.length -1; i>=0;i--){
+   r = r + str[i];
+  }
+  return r;
+}
+  
+  console.log(reverseString("hello"));
